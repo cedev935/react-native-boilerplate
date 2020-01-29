311251bf0937695ef1b5ce7cb279c0a9ed0be42c
Salvatore Zicaro <salvatore.zicaro@isobar.com>
2020-01-29 12:48:10 +0100

    create simple navigation

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import {NavigationNativeContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
-function HomeScreen() {
+function DetailsScreen() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
+      <Text>Details!</Text>
     </View>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
   );
 }
 
@@ -46,7 +71,7 @@ export default function App() {
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
         }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationNativeContainer>
